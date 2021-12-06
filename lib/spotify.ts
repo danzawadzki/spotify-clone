5bef9346726aaeffa3444af42dee27565d68e535
Daniel Zawadzki <dan@ggpredict.io>
2021-12-06 21:36:33 +0100

    Added basic spotifyApi config.

---
+++ lib/spotify.ts
@@ -0,0 +1,28 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+const SCOPES = [
+  "user-read-email",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "user-read-email",
+  "streaming",
+  "user-read-private",
+  "user-library-read",
+  "user-top-read",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-follow-read",
+].join(",");
+
+const queryParams = new URLSearchParams({
+  scope: SCOPES,
+});
+
+export const SPOTIFY_LOGIN_URL = `https://accounts.spotify.com/authorize?${queryParams.toString()}`;
+
+export const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPORIFY_ID,
+  clientSecret: process.env.SPORIFY_SECRET,
+});
