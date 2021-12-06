6cef9555b5b143d1e09afb211bda515e75526be8
Daniel Zawadzki <dan@ggpredict.io>
2021-12-06 22:21:18 +0100

    Updated tailwind theme and added basic styling to the login page.

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from "react";
-import { getProviders } from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 import { ClientSafeProvider, LiteralUnion } from "next-auth/react/types";
 import { BuiltInProviderType } from "next-auth/providers";
 
@@ -11,7 +11,23 @@ type LoginProps = {
 };
 
 const Login: FunctionComponent<LoginProps> = ({ providers }) => (
-  <div>{"Im just regular login page"}</div>
+  <div className="flex flex-col align-center justify-center items-center bg-black min-h-screen w-full">
+    <img
+      src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Green.png"
+      className="w-72 mb-5"
+      alt="Spotify logo"
+    />
+    {Object.values(providers).map((provider) => (
+      <div key={provider.name}>
+        <button
+          className="bg-green-500 hover:bg-green-700 transition duration-100 ease-in-out text-white rounded-full py-2 px-6"
+          onClick={() => signIn(provider.id)}
+        >
+          Login with {provider.name}
+        </button>
+      </div>
+    ))}
+  </div>
 );
 
 export default Login;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./shared/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...colors,
+      green: {
+        ...colors.green,
+        default: "#18D860",
+      },
+    },
     extend: {},
   },
   variants: {
