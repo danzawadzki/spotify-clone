d8ab208c3e6ca9071741dcd8696f62c21cce99ef
Daniel Zawadzki <dan@ggpredict.io>
2021-12-06 17:17:30 +0100

    Added new links to the sidebar.

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Sidebar from "../shared/Sidebar/Sidebar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="bg-black h-screen overflow-hidden">
       <Head>
         <title>Music player</title>
         {/*<link rel="icon" href="/favicon.ico" />*/}
--- shared/Sidebar/Sidebar.tsx
+++ shared/Sidebar/Sidebar.tsx
@@ -1,7 +1,48 @@
-import react, { FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
+import {
+  HeartIcon,
+  HomeIcon,
+  LibraryIcon,
+  PlusIcon,
+  RssIcon,
+  SearchIcon,
+} from "@heroicons/react/outline";
+
+const NAV_BUTTONS = [
+  { label: "Home", icon: HomeIcon },
+  { label: "Search", icon: SearchIcon },
+  { label: "Your library", icon: LibraryIcon },
+];
+
+const YOUR_BUTTONS = [
+  { label: "Create Playlist", icon: PlusIcon },
+  { label: "Liked Songs", icon: HeartIcon },
+  { label: "Your Episode", icon: RssIcon },
+];
 
 type SidebarProps = {};
 
-const Sidebar: FunctionComponent<SidebarProps> = ({}) => <div />;
+const Sidebar: FunctionComponent<SidebarProps> = ({}) => {
+  return (
+    <div className="p-5 text-gray-500 text-sm border-r border-gray-900">
+      <div className="space-y-4">
+        {NAV_BUTTONS.map(({ label, icon: Icon }) => (
+          <button className="flex items-center space-x-2 hover:text-white">
+            <Icon className="w-10 h-5" />
+            <p>{label}</p>
+          </button>
+        ))}
+        <hr className="border-t-[0.1px] border-gray-900" />
+        {YOUR_BUTTONS.map(({ label, icon: Icon }) => (
+          <button className="flex items-center space-x-2 hover:text-white">
+            <Icon className="w-10 h-5" />
+            <p>{label}</p>
+          </button>
+        ))}
+        <hr className="border-t-[0.1px] border-gray-900" />
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
