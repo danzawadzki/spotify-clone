2ea75f0e9889f36791db43dcae274529d294d2e4
Daniel Zawadzki <hello@danielzawadzki.com>
2021-12-19 23:00:35 +0100

    Updated songs styling.

--- shared/Songs/Song.tsx
+++ shared/Songs/Song.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from "react";
+import { msToMins } from "../../lib/time";
 
 type SongProps = {
   track: SpotifyApi.PlaylistTrackObject;
@@ -7,7 +8,7 @@ type SongProps = {
 
 const Song: FunctionComponent<SongProps> = ({ track, order }) => {
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
       <div className="flex items-center space-x-4">
         <p>{order}</p>
         <img
@@ -16,13 +17,13 @@ const Song: FunctionComponent<SongProps> = ({ track, order }) => {
           className="w-10 h-10"
         />
         <div>
-          <p>{track.track.name}</p>
-          <p>{track.track.artists[0].name}</p>
+          <p className="w-36 lg:w-64 truncate text-white">{track.track.name}</p>
+          <p className="w-40 truncate">{track.track.artists[0].name}</p>
         </div>
-        <div className="flex inline-center justify-between ml-auto md:ml-0">
-          <p className="hidden: md:inline">{track.track.album.name}</p>
-          <p>duration</p>
-        </div>
+      </div>
+      <div className="flex items-center justify-between ml-auto md:ml-0">
+        <p className="w-40 hidden: md:inline">{track.track.album.name}</p>
+        <p>{msToMins(track.track.duration_ms)}</p>
       </div>
     </div>
   );
